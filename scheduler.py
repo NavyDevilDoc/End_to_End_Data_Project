4e6c94346a915837f0bccfdb8e3b082feef2e24d
NavyDevilDoc <79477977+NavyDevilDoc@users.noreply.github.com>
2023-09-08 14:34:41 -0400

    Update scheduler.py
    Updated to correct error with def sample_and_analyze()

--- scheduler.py
+++ scheduler.py
@@ -11,8 +11,10 @@ db_name = config['database']['name']
 
 def sample_and_analyze():
     sample = database_operations.sample_data(db_name, 50)
-    analysis_result = data_analysis.your_analysis_function(sample)
-    print("Analysis result:", analysis_result)  # or log it
+    if sample is not None:
+        analysis_result = data_analysis.perform_eda_and_plot(sample)
+        print("Analysis result:", analysis_result)  # or log it
+
 
 # Initialize Scheduler
 schedule.every(1).hours.do(sample_and_analyze)
